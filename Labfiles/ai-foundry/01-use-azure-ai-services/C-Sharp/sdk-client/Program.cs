1a43ef9254a2ab77dfee2cc341afcbe48f154b2e
Alison Felix <felix.alison@outlook.com>
2025-03-19 18:12:35 +0000

    Duplicated Labfiles folders into ai-foundry folder

---
+++ Labfiles/ai-foundry/01-use-azure-ai-services/C-Sharp/sdk-client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Azure;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using Azure.AI.TextAnalytics;
+
+namespace sdk_client
+{
+    class Program
+    {
+
+        private static string AISvcEndpoint;
+        private static string AISvcKey;
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                AISvcEndpoint = configuration["AIServicesEndpoint"];
+                AISvcKey = configuration["AIServicesKey"];
+
+                // Get user input (until they enter "quit")
+                string userText = "";
+                while (userText.ToLower() != "quit")
+                {
+                    Console.WriteLine("\nEnter some text ('quit' to stop)");
+                    userText = Console.ReadLine();
+                    if (userText.ToLower() != "quit")
+                    {
+                        // Call function to detect language
+                        string language = GetLanguage(userText);
+                        Console.WriteLine("Language: " + language);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static string GetLanguage(string text)
+        {
+
+            // Create client using endpoint and key
+            AzureKeyCredential credentials = new AzureKeyCredential(AISvcKey);
+            Uri endpoint = new Uri(AISvcEndpoint);
+            var client = new TextAnalyticsClient(endpoint, credentials);
+
+            // Call the service to get the detected language
+            DetectedLanguage detectedLanguage = client.DetectLanguage(text);
+            return(detectedLanguage.Name);
+
+        }
+    }
+}
